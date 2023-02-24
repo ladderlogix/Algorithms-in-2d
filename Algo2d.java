1abe3d407084ab33a5d5e060a70ed5c58e955e4f
Stephen Glombicki <9000050@student.munster.us>
2023-02-24 09:24:58 -0600

    Create Algo2d.java

---
+++ Algo2d.java
@@ -0,0 +1,23 @@
+public class Algo2d {
+    public int FindHighest(int[][] array){
+        int highest = array[0][0];
+        for(int[] x : array){
+            for(int y : x){
+                if(y > highest){
+                    y = highest;
+                }
+            }
+        }
+        return highest;
+    }
+    
+    public int Accumulate(int[][] array){
+        int sum = 0;
+        for(int[] x : array){
+            for(int y : x){
+                sum += y;
+            }
+        }
+        return sum;
+    }
+}
